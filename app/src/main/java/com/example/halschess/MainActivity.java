9baad18d9914b6f0e401c937315b9614c6a35da7
Kaden Carr <kccarr6022@eagle.fgcu.edu>
2022-07-24 06:40:00 -0400

    Pawn and castle movement

--- app/src/main/java/com/example/halschess/MainActivity.java
+++ app/src/main/java/com/example/halschess/MainActivity.java
@@ -6,9 +6,196 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    String[][] board = new String[8][8]; // The surrounding contains nullptr/ empty memory
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        resetBoard(board);
+
     }
+
+
+
+
+    public void resetBoard( String[][] board) {
+        /* The board in this model will be 9 by 9 and will have pieces arranged correctly. */
+
+        board =
+                new String[][]{ // lowercase = black uppercase = white
+                        {"c", "n", "b", "q", "k", "b", "n", "c"}, // 8
+                        {"p", "p", "p", "p", "p", "p", "p", "p"}, // 7
+                        {"0", "0", "0", "0", "0", "0", "0", "0"}, // 6
+                        {"0", "0", "0", "0", "0", "0", "0", "0"}, // 5
+                        {"0", "0", "0", "0", "0", "0", "0", "0"}, // 4
+                        {"0", "0", "0", "0", "0", "0", "0", "0"}, // 3
+                        {"P", "P", "P", "P", "P", "P", "P", "P"}, // 2
+                        {"C", "N", "B", "Q", "K", "B", "N", "C"}  // 1
+                }; //     A    B    C    D    E    F    G    H
+
+
+    }
+
+    public boolean move(String before, String after) { // Takes two (A-H)(1-8) strings
+        int beforex = Character.toUpperCase(getCharFromString(before, 0)) - 'A';
+        int beforey = getCharFromString(before, 1) - '1';
+        int afterx = Character.toUpperCase(getCharFromString(after, 0)) - 'A';
+        int aftery = getCharFromString(after, 1) - '1';
+        char piece = getCharFromString(board[beforex][beforey], 0);
+
+        // white
+        if (Character.isUpperCase(piece)  &&
+                (!Character.isUpperCase(getCharFromString(board[afterx][aftery], 0)))){
+
+            // pawns
+            if (board[beforex][beforey].equals("P")) {
+                // forward
+                for (int i = 0; i <= 2; i++) {
+                    if (beforex == afterx && aftery == beforey + i
+                            && board[beforex][beforey].equals("0")) {
+                        board[beforex][beforey] = "0";
+                        board[afterx][aftery] = "P";
+                        return true;
+                    }
+                }
+                // diagonal take
+                if ((beforex + 1 == afterx || beforex - 1 == afterx) && beforey + 1 == aftery) {
+                    board[beforex][beforey] = "0";
+                    board[afterx][aftery] = "P";
+                    return true;
+                }
+            }
+
+            // castles
+            if (board[beforex][beforey].equals("C")) {
+                for (int i = 0; i <= 8; i++) {
+                    // forward
+                    if (beforey + i == aftery) {
+                        board[beforex][beforey] = "0";
+                        board[afterx][aftery] = "C";
+                        return true;
+                    }
+                    // backward
+                    if (beforey - i == aftery) {
+                        board[beforex][beforey] = "0";
+                        board[afterx][aftery] = "C";
+                        return true;
+                    }
+                    // right
+                    if (beforex + i == aftery) {
+                        board[beforex][beforey] = "0";
+                        board[afterx][aftery] = "C";
+                        return true;
+                    }
+                    // left
+                    if (beforex - i == aftery) {
+                        board[beforex][beforey] = "0";
+                        board[afterx][aftery] = "C";
+                        return true;
+                    }
+                }
+            }
+
+            // knights
+
+
+            // bishops
+
+            // queens
+
+            // kings
+
+            // nothing (out of bounds) <- buttons(unlikely)
+
+        // black
+        } else if (Character.isLowerCase(piece)){
+
+            // pawns
+            if (board[beforex][beforey] == "p") {
+                // forward
+                for (int i = 0; i <= 2; i--) {
+                    if (beforex == afterx && aftery == beforey + i
+                            && board[beforex][beforey].equals("0")) {
+                        board[beforex][beforey] = "0";
+                        board[afterx][aftery] = "P";
+                        return true;
+                    }
+                }
+                // diagonal take
+                if ((beforex + 1 == afterx || beforex - 1 == afterx) && beforey - 1 == aftery) {
+                    board[beforex][beforey] = "0";
+                    board[afterx][aftery] = "P";
+                    return true;
+                }
+            }
+
+            // castles
+            if (board[beforex][beforey].equals("c")) {
+                for (int i = 0; i <= 8; i++) {
+                    // forward
+                    if (beforey + i == aftery) {
+                        board[beforex][beforey] = "0";
+                        board[afterx][aftery] = "c";
+                        return true;
+                    }
+                    // backward
+                    if (beforey - i == aftery) {
+                        board[beforex][beforey] = "0";
+                        board[afterx][aftery] = "c";
+                        return true;
+                    }
+                    // right
+                    if (beforex + i == aftery) {
+                        board[beforex][beforey] = "0";
+                        board[afterx][aftery] = "c";
+                        return true;
+                    }
+                    // left
+                    if (beforex - i == aftery) {
+                        board[beforex][beforey] = "0";
+                        board[afterx][aftery] = "c";
+                        return true;
+                    }
+                }
+            }
+
+            // knights
+
+            // bishops
+
+            // queens
+
+            // kings
+
+            // nothing (out of bounds) <- buttons(unlikely)
+        }
+        else {
+            "".toString();
+        }
+        return false;
+    }
+
+    public boolean checkWin() {
+        return true;
+    }
+
+    public boolean gameOver() {
+        return true;
+    }
+
+
+
+
+
+    public static char
+    getCharFromString(String str, int index)
+    {
+        return str.charAt(index);
+    }
+
+
 }
